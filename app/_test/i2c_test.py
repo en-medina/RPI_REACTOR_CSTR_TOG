4a6c9e7edee95872380f4a72467e28ace2fa7466
Enmanuel Medina <em.enmanuelmarcial@gmail.com>
2020-07-03 22:53:31 -0400

    Fix I2CIFACE methods internal implementation

---
+++ app/_test/i2c_test.py
@@ -0,0 +1,15 @@
+import importlib.util
+
+spec = importlib.util.spec_from_file_location("i2c", "../i2c_service/i2c.py")
+i2c = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(i2c)
+
+spec = importlib.util.spec_from_file_location("MLX90614", "../i2c_service/mlx90614.py")
+mlx90614 = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(mlx90614)
+
+if '__main__' == __name__:
+	i2cIface = i2c.I2CIface()
+	temperature1 = mlx90614.MLX90614(i2cIface, 'reactive1_temperature', 0x5b)
+	for _ in range(10):
+		print('the temperature is:', temperature1.get_ambient())
--- app/i2c_service/i2c.py
+++ app/i2c_service/i2c.py
@@ -3,7 +3,16 @@
 import logging
 from time import sleep
 import threading
-import shared_module.singleton as singleton
+
+try:
+    import shared_module.singleton as singleton
+except Exception:
+    import importlib.util
+
+    spec = importlib.util.spec_from_file_location("Singleton", "../shared_module/singleton.py")
+    singleton = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(singleton)
+
 try:
     import smbus
 except Exception:
@@ -16,6 +25,7 @@ class I2CIface(metaclass=singleton.Singleton):
     __internalDelay = 0.001
     def __init__():
         bus = smbus.SMBus(config['i2c']['channel'])
+
     def action(self, data):
         with self.__internalLock:
             sleep(__internalDelay)
@@ -29,33 +39,35 @@ class I2CIface(metaclass=singleton.Singleton):
         with self.__internalLock:
             sleep(__internalDelay)
             print('read_byte')
-            #self.bus.read_i2c_block_data(address, register)
+            self.bus.read_byte_data(address, register)
         pass
 
     def write_byte(self, address, msg, register = None):
         with self.__internalLock:
             sleep(__internalDelay)
             print('write_byte')
-            #self.bus.write_i2c_block_data(address, register, msg)
+            self.bus.write_byte_data(address, register, msg)
         pass
 
     def write_word_data(self, address, msg, register):
         with self.__internalLock:
             sleep(__internalDelay)
             print('write_byte')
-            #self.bus.write_word_data(address, register, msg)
-        pass
-    def write_buffer(self, address, buffer, register):
-        with self.__internalLock:
-            for msg in buffer:
-                #self.bus.write_word_data(address, register, msg)
-                pass
+            self.bus.write_word_data(address, register, msg)
         pass
 
     def read_word_data(self, address, register):
         with self.__internalLock:
             sleep(__internalDelay)
             print('read_word')
-            #return self.bus.read_word_data(address, register)
+            return self.bus.read_word_data(address, register)
         pass
+
+    def write_buffer(self, address, buffer, register):
+        with self.__internalLock:
+            for msg in buffer:
+                self.bus.write_word_data(address, register, msg)
+                pass
+        pass
+
 
