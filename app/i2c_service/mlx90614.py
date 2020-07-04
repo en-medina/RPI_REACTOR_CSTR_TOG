30a69811dea4aeae7be5efbd9531eac1f7b755ea
Enmanuel Medina <em.enmanuelmarcial@gmail.com>
2020-07-03 23:33:50 -0400

    fix mlx90614 module object1 temperature

--- app/i2c_service/mlx90614.py
+++ app/i2c_service/mlx90614.py
@@ -65,7 +65,7 @@ class MLX90614():
     def read_temp(self, reg):
         data = self.read_reg(reg)
         temp = (data * 0.02) - 273.15
-        return temp
+        return round(temp,2)
 
     @property
     def measure(self):
@@ -80,7 +80,9 @@ class MLX90614():
         return self.get_ambient()
 
     def get_ambient(self):
-        return self.read_temp(self.MLX90614_TA)
+        data = self.read_temp(self.MLX90614_TA)
+        temp = (data * 0.02) - 273.15
+        return round(temp, 2)
 
     def get_object_1(self):
         return self.read_temp(self.MLX90614_TOBJ1)
