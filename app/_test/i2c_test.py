fe1aba2d0805286600c7a3bc526eb6dcda41a32e
Enmanuel Medina <em.enmanuelmarcial@gmail.com>
2020-07-03 23:06:54 -0400

    Fix MLX90614 test config parameter

--- app/_test/i2c_test.py
+++ app/_test/i2c_test.py
@@ -9,7 +9,8 @@ mlx90614 = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(mlx90614)
 
 if '__main__' == __name__:
-	i2cIface = i2c.I2CIface()
+	config = {'i2c':{'channel':1}}
+	i2cIface = i2c.I2CIface(config)
 	temperature1 = mlx90614.MLX90614(i2cIface, 'reactive1_temperature', 0x5b)
 	for _ in range(10):
 		print('the temperature is:', temperature1.get_ambient())
