3bdcd4f03633744d5ef5fadaba349de6b5aeb3b8
Enmanuel <em.enmanuelmarcial@gmail.com>
2020-07-15 12:10:59 -0400

    Fix HCSR04 Class internal attribute call error

--- app/dist_service/hcsr04.py
+++ app/dist_service/hcsr04.py
@@ -22,21 +22,21 @@ class HCSR04():
 
 	def distance(self):
 		# set Trigger to HIGH
-		GPIO.output(GPIO_TRIGGER, True)
+		GPIO.output(self.GPIO_TRIGGER, True)
 
 		# set Trigger after 0.01ms to LOW
 		time.sleep(0.00001)
-		GPIO.output(GPIO_TRIGGER, False)
+		GPIO.output(self.GPIO_TRIGGER, False)
 
 		StartTime = time.time()
 		StopTime = time.time()
 
 		# save StartTime
-		while GPIO.input(GPIO_ECHO) == 0:
+		while GPIO.input(self.GPIO_ECHO) == 0:
 		    StartTime = time.time()
 
 		# save time of arrival
-		while GPIO.input(GPIO_ECHO) == 1:
+		while GPIO.input(self.GPIO_ECHO) == 1:
 		    StopTime = time.time()
 
 		# time difference between start and arrival
