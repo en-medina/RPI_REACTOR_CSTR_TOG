70f202ba140c0b6dea5cc53240fadc0d7a23b3ab
Enmanuel Medina <em.enmanuelmarcial@gmail.com>
2020-07-17 18:18:46 -0400

    Fix IP Address search by helper

--- app/shared_module/helpers.py
+++ app/shared_module/helpers.py
@@ -76,6 +76,8 @@ def get_server_ip():
 
     :return string: the server IP address
     """
-    hostname = socket.gethostname()
-    serverIP = socket.gethostbyname(hostname)
+    serverIP = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] 
+    if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), 
+    s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, 
+    socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
     return serverIP
