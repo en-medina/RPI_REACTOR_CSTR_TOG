c64e797488c0e3baaa6b3dd1c6c125668ff391c3
Enmanuel Medina <em.enmanuelmarcial@gmail.com>
2020-07-23 11:43:00 -0400

    Add ligth test

--- app/_test/test_lights.py
+++ app/_test/test_lights.py
@@ -3,11 +3,11 @@ GPIO.cleanup()
 GPIO.mode(GPIO.BOARD)
 pins = [33, 31, 29, 32, 13]
 try:
-	for pin in pins
+	for pin in pins:
 		GPIO.setup(pin, GPIO.OUT)
 		GPIO.output(self.GPIO_TRIGGER, GPIO.HIGH)
 	sleep(10)
-	for pin in pins
+	for pin in pins:
 		GPIO.output(self.GPIO_TRIGGER, GPIO.LOW)
 except Exception:
 	GPIO.cleanup()
