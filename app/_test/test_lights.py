ea80d57790fe7234ed2608a85dd5aa93d0137cc5
Enmanuel Medina <em.enmanuelmarcial@gmail.com>
2020-07-23 11:40:32 -0400

    Add ligth test

---
+++ app/_test/test_lights.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO  
+GPIO.cleanup() 
+GPIO.mode(GPIO.BOARD)
+pins = [33, 31, 29, 32, 13]
+try:
+	for pin in pins
+		GPIO.setup(pin, GPIO.OUT)
+		GPIO.output(self.GPIO_TRIGGER, GPIO.HIGH)
+	sleep(10)
+	for pin in pins
+		GPIO.output(self.GPIO_TRIGGER, GPIO.LOW)
+except Exception:
+	GPIO.cleanup()
+GPIO.cleanup()
