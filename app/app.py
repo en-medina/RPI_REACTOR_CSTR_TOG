af1528d4130dce0e348c54b6ba30a26b09d87f9f
Enmanuel Medina <em.enmanuelmarcial@gmail.com>
2020-07-22 20:25:43 -0400

    Add GPIO module

--- app/app.py
+++ app/app.py
@@ -3,6 +3,11 @@ import logging
 logging.basicConfig(level=logging.INFO,
             #format= '[%(levelname)s] [%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
             format='[%(levelname)s] (%(threadName)-10s) %(message)s')
+try:
+    import RPi.GPIO as GPIO
+except Exception:
+  logging.critical("RPi.GPIO package not found... creating None variable for testing purpose only...")
+  GPIO = None
 import threading
 import time
 import queue
@@ -51,13 +56,16 @@ if __name__ == "__main__":
     #pipeline['web']['bus'] = pipeline['bus']['bus']
     #pipeline['bus']['web'] = pipeline['web']['web']
 
+    if not GPIO is None:
+        logging.info("Changing GPIO to BOARD MODE")
+        GPIO.setmode(GPIO.BOARD) # BOARD pin-numbering scheme
 
     logging.info('Starting REACTOR CSTR - TOG Application Services...')
 
     logging.info(f'showing sensor names list{get_device_names()}')
     with concurrent.futures.ThreadPoolExecutor(max_workers=5, thread_name_prefix = 'app-service') as executor:
         futureException = {
-              #executor.submit(init_bit, pipeline): 'bit-service',
+              executor.submit(init_bit, pipeline): 'bit-service',
               executor.submit(init_web, pipeline): 'web-service',
               executor.submit(init_i2c, pipeline): 'i2c-service',
               executor.submit(init_dist, pipeline): 'dist-service',
@@ -71,5 +79,5 @@ if __name__ == "__main__":
                 logging.error(f'<{threadName}> generated an exception: {exc}')
             else:
                 logging.info(f'<{threadName}> finish without error and return \'{data}\'')
-
+    GPIO.cleanup()
     
