7a0f44ac8a6be080a611a875b52d9cb0b2481d6a
Enmanuel Medina <em.enmanuelmarcial@gmail.com>
2020-07-23 11:47:53 -0400

    Fix error ligth test

--- app/_test/test_lights.py
+++ app/_test/test_lights.py
@@ -5,10 +5,10 @@ pins = [33, 31, 29, 32, 13]
 try:
 	for pin in pins:
 		GPIO.setup(pin, GPIO.OUT)
-		GPIO.output(self.GPIO_TRIGGER, GPIO.HIGH)
+		GPIO.output(pin, GPIO.HIGH)
 	sleep(10)
 	for pin in pins:
-		GPIO.output(self.GPIO_TRIGGER, GPIO.LOW)
+		GPIO.output(pin, GPIO.LOW)
 except Exception as e:
 	print(e)
 	GPIO.cleanup()
