11076b45c0e3da05199a633dcca26f0f5aeceab5
Enmanuel Medina <em.enmanuelmarcial@gmail.com>
2020-07-24 10:05:25 -0400

    Fix bulb bit logic

--- app/_test/test_lights.py
+++ app/_test/test_lights.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BOARD) # BOARD pin-numbering scheme
 GPIO.setwarnings(False)
-pins = [33, 31]# 29, 32, 13]
+pins = [33, 31, 29] #32, 13]
 try:
 	for pin in pins:
 		GPIO.setup(pin, GPIO.OUT)
