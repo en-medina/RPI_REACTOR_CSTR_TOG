7c03faf945be1282d039973f3541faad7615c37f
Enmanuel Medina <em.enmanuelmarcial@gmail.com>
2020-07-26 22:11:36 -0400

    Change Light Test script

--- app/_test/test_lights.py
+++ app/_test/test_lights.py
@@ -6,10 +6,12 @@ pins = [33, 31, 29, 32, 13]
 try:
 	for pin in pins:
 		GPIO.setup(pin, GPIO.OUT)
-		GPIO.output(pin, GPIO.LOW)
-	sleep(10)
-	for pin in pins:
-		GPIO.output(pin, GPIO.HIGH)
+	for _ in range(2):
+		for pin in pins:
+			GPIO.output(pin, GPIO.HIGH)
+		sleep(3)
+		for pin in pins:
+			GPIO.output(pin, GPIO.LOW)
 except Exception as e:
 	print(e)
 	GPIO.cleanup()
