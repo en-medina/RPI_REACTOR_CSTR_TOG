b5ede0cd4d42815f0e33cd47bcde309d65f41f3b
Enmanuel Medina <em.enmanuelmarcial@gmail.com>
2020-08-03 21:01:03 -0400

    Fix get_server_ip non IP error on helpers.py

--- app/shared_module/helpers.py
+++ app/shared_module/helpers.py
@@ -76,8 +76,13 @@ def get_server_ip():
 
     :return string: the server IP address
     """
-    serverIP = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] 
-    if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), 
-    s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, 
-    socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+    serverIP = 'localhost'
+    try:
+        serverIP = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] 
+        if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), 
+        s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, 
+        socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+    except Exception:
+        searching = 'localhost'
+        pass
     return serverIP
