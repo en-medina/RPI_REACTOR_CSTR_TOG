ab87ee1c2d85b2c84a83023ecb4ee37b8db7998d
Enmanuel Medina <em.enmanuelmarcial@gmail.com>
2020-09-06 16:30:27 -0400

    Uncomment Flow measurement

--- app/web_service/forms.py
+++ app/web_service/forms.py
@@ -12,8 +12,7 @@ class LimitForm(FlaskForm):
 	def validate(self):
 		if not Form.validate(self):
 			return False
-		return self.delay.data >= 0 and self.loTemp.data < self.hiTemp.data 
-		#	and self.loSpeed.data < self.hiSpeed.data
+		return self.delay.data >= 0 and self.loTemp.data < self.hiTemp.data and self.loSpeed.data < self.hiSpeed.data
 
 class GraphForm(FlaskForm):
 	beginDate = DateField('begin_date')
@@ -36,6 +35,6 @@ class GraphForm(FlaskForm):
 			"agitator_speed": "Velocidad Agitador",
 			"adc": "adc"
 		}
-		self.sensorList.choices = [(x, names[x]) for x in db.get_table_name() if not 'speed' in x and not 'flow' in x]
-		self.intervalList.choices = [(1,"1seg"),(10,"10seg"),(15,"15seg"),(60,"1min"),(600,"10min"),(900,"15min"),(1800,"30min"),(3600,"1h"),(10800,"3h"),(43200,"12h")]
+		self.sensorList.choices = [(x, names[x]) for x in db.get_table_name()]
+		self.intervalList.choices = [(60,"1min"),(600,"10min"),(900,"15min"),(1800,"30min"),(3600,"1h"),(10800,"3h"),(43200,"12h")]
 
